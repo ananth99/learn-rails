c1e14060b92f31998ffbdeaea9c13504aa8de81f
Ananth Madhavan <ananth@desto.co.in>
2014-05-31 05:10:51 +0530

    Update application layout.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,8 @@
 class VisitorsController < ApplicationController
 	def new
-		@owner = Owner.new;
+		@owner = Owner.new
+		flash.now[:notice] = "welcome!"
+		flash.now[:alert] = "My Birthday is soon!"
 	end
 
 end
